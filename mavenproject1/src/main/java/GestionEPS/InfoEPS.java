0e5f9c5695c5673f874c26379235a5bc63b35f44
Marco <markos.avila@gmail.com>
2021-09-25 01:38:07 -0500

    Se agrega gestion de medicos, funcionarios y pacientes

--- mavenproject1/src/main/java/GestionEPS/InfoEPS.java
+++ mavenproject1/src/main/java/GestionEPS/InfoEPS.java
@@ -11,4 +11,69 @@ package GestionEPS;
  */
 public class InfoEPS {
     
+    private int nit;
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private String email;
+
+    public InfoEPS() {
+    }
+
+    public InfoEPS(int nit, String nombre, String direccion, int telefono, String email) {
+        this.nit = nit;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "InfoEPS{" + "nit=" + nit + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + ", email=" + email + '}';
+    }
+    
+    
+    
+    
 }
